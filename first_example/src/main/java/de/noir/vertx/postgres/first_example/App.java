f272d1a8de366ef18b2fb33ec9867ae4d573f865
MasterNoir <c.apetz@t-online.de>
2017-05-07 12:56:18 +0200

    Projekt erstellt

---
+++ first_example/src/main/java/de/noir/vertx/postgres/first_example/App.java
@@ -0,0 +1,13 @@
+package de.noir.vertx.postgres.first_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
