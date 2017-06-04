f503a73b3d6e97a8ab9c6e0d95c661278f7ff350
MasterNoir <c.apetz@t-online.de>
2017-06-04 19:21:58 +0200

    Start-Funktion dynamisch aufgebaut

---
+++ first_example/src/main/java/de/noir/vertx/postgres/first_example/Start.java
@@ -0,0 +1,107 @@
+package de.noir.vertx.postgres.first_example;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import com.hazelcast.config.Config;
+
+import de.noir.vertx.postgres.first_example.client.Client;
+import de.noir.vertx.postgres.first_example.server.Server;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+
+public class Start implements ActionListener{
+	
+	JFrame cFrame;
+	JButton clientButton;
+	JButton serverButton;
+	
+	
+	/*
+		Hier werden alle notwendigen Einstellungen getroffen um eine Vert.x-Instanz zu erstellen.
+		Es wird TcpIp verwendet und Multicast deaktiviert.
+	 */
+
+    public static void main(String[] args) {
+    	
+    	Start start = new Start();
+    	start.initGUI();
+
+    }    
+    
+    public void initGUI(){
+    	
+    	System.out.println("test");
+    	JFrame.setDefaultLookAndFeelDecorated(true);
+        cFrame = new JFrame();
+        cFrame.setTitle("Client");      
+        cFrame.setSize(250,75);
+        cFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JPanel container = new JPanel();
+        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+
+        JPanel panel1 = new JPanel();
+
+        container.add(panel1);
+        
+        clientButton = new JButton("Start Client");
+        clientButton.addActionListener(this);
+        panel1.add(clientButton);
+        
+        serverButton = new JButton("Start Server");
+        serverButton.addActionListener(this);
+        panel1.add(serverButton);
+        
+        cFrame.add(container);
+        cFrame.setVisible(true);
+    }
+    
+    public void actionPerformed (ActionEvent ae){
+    	
+    	// Erstellt eine Config-Variable
+    	Config hazelcastConfig = new Config();
+    	// Aktiviert TcpIp und bestimmt die Adressen.
+        //hazelcastConfig.getNetworkConfig().getJoin().getTcpIpConfig().addMember("192.168.2.112").setEnabled(true);
+    	hazelcastConfig.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(true);
+        // Deaktiviert Multicast.
+        hazelcastConfig.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+        
+        // Verbindet die Config mit dem Hazelcast-ClusterManager
+        ClusterManager mgr = new HazelcastClusterManager(hazelcastConfig);
+        
+        // Setzt die Vertx-Optionen auf Cluster-Modus und verwendet den konfigurierten Hazelcast-CM. 
+    	//VertxOptions options = new VertxOptions().setClustered(true).setClusterManager(mgr).setClusterHost("192.168.178.52");
+    	VertxOptions options = new VertxOptions().setClustered(true).setClusterManager(mgr).setClusterHost("192.168.2.118");
+    	
+    	Vertx.clusteredVertx(options, res -> {
+    		
+    		cFrame.dispose();
+    		
+            if (res.succeeded()) {
+              Vertx vertx = res.result();
+              
+              if(ae.getSource() == this.serverButton){
+            	  vertx.deployVerticle(new Server());
+              }else if(ae.getSource() == this.clientButton){
+            	  vertx.deployVerticle(new Client());
+              }
+              
+            } else {
+              System.out.println("Clustered Vertx failed!");
+            }
+          });
+    	
+    	
+    }
+
+}
+
+
